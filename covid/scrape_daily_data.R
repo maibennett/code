bc151beb58ed25b5235604e959f4d7e344331935
Magdalena Bennett <mb3863@tc.columbia.edu>
2020-04-03 14:06:27 -0400

    Update scrape_daily_data.R

--- covid/scrape_daily_data.R
+++ covid/scrape_daily_data.R
@@ -10,15 +10,15 @@ library(stringr)
 library(pdftools)
 library(rvest)
 
-url <- read_html("https://www.minsal.cl/nuevo-coronavirus-2019-ncov/casos-confirmados-en-chile-covid-19/")
+url <- read_html("https://www.minsal.cl/nuevo-coronavirus-2019-ncov/casos-confirmados-en-chile-covid-19/") #This is the website we want to scrape
 
 regions = c("Arica y Parinacota","Tarapaca","Antofagasta","Atacama", "Coquimbo", 
             "Valparaiso", "Metropolitana","Ohiggins", "Maule","Nuble","Biobio",
             "Araucania","Los Rios","Los Lagos","Aysen","Magallanes")
 
-covid_count_table <- html_nodes(url, "table") %>%
+covid_count_table <- html_nodes(url, "table") %>% #In this case, the data we want to get is in <table> </table>
   .[[1]] %>%
-  html_table(fill=TRUE) %>%
+  html_table(fill=TRUE) %>% 
   rename(Region = X1, NewCases = X2,TotalCases = X3,Deaths = X5) %>% #Give them better names
   filter(Region!="",Region!="Total",Deaths!="Fallecidos") %>% #Filter rows that we don't need
   mutate(Region = regions) %>% #Rename regions so they are equivalent to other files
@@ -26,14 +26,13 @@ covid_count_table <- html_nodes(url, "table") %>%
   select(Region:NewCases) #Just select the two columns we need
 
 #Now we need to include the date:
-
 #Date from 1st case:
 first_date = as.Date("2020-03-03")
 
-date = html_nodes(url, "p")%>%
+date = html_nodes(url, "p")%>% #The date for the report is given under the table betweeb <p></p>. To know which nodes to select, you can use (Right Click - Inspect) on your browser to see the html code (if, as me, you are not as familiar with this)
   .[[1]] %>%
-  html_text() %>%
-  str_match("Informe corresponde al (.*?). El")
+  html_text() %>% #this is just text
+  str_match("Informe corresponde al (.*?). El") #Extracts the date that the report was updated, which is located between these two strings
 
 date = date[,2]
 
@@ -42,17 +41,17 @@ day = as.numeric(date_char[,2])
 
 months = c("enero","febrero","marzo","abril","mayo","junio","julio",
            "agosto","septiembre","octubre","noviembre","diciembre")
-month = which(months == date_char[,3])
+month = which(months == date_char[,3]) #Months are in Spanish, so we convert them to numbers
 
+update_date = as.Date(paste("2020",month,day,sep="-")) #Date for the update
 
-update_date = as.Date(paste("2020",month,day,sep="-"))
-
-days = update_date - first_date
+days = update_date - first_date #Days since first case
 
 #Load Data
 d = read.csv("https://raw.githubusercontent.com/maibennett/code/master/covid/data_covid_region.csv",
-             stringsAsFactors = FALSE)
+             stringsAsFactors = FALSE) #Read the data we already had
 
+#Build updated data in the format of the previous data
 d_update = cbind(rep(days,nrow(covid_count_table)),covid_count_table)
 names(d_update) = c("day","region","n_obs")
 
@@ -66,7 +65,7 @@ for(i in 1:16){
   d_update$region_lon[d_update$region==regions[i]] = mean(d$region_lon[d$region==regions[i]])
 }
 
-#drop regions with no new cases:
+#drop regions with no new cases: (not necessary)
 d_update = d_update[d_update$n_obs!=0,]
 
 #Only update if there's new data:
