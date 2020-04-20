0a9dea99dc78ad4249175d84825d82d6d4294089
Magdalena Bennett <mb3863@tc.columbia.edu>
2020-04-20 11:22:38 -0400

    Update scrape_daily_reports_pdf.py

--- covid/code/scrape_daily_reports_pdf.py
+++ covid/code/scrape_daily_reports_pdf.py
@@ -15,10 +15,10 @@ import unidecode
 import csv
 import gc
 
-gc.collect()
+## Disclaimer: Format of reports have been changing, so this code might need to get slightly adjusted to reflect some of these changes in the future.
 
 ###### Change the date and the version of the report here
-date = 20200410
+date = 20200420
 #version = "v3"
 version = ""
 ##########################################################
@@ -34,7 +34,7 @@ DailyReports = tabula.read_pdf(url,pages='all')
 
 # For tests:
 all_tests = pd.read_csv('https://raw.githubusercontent.com/maibennett/code/master/covid/data/tests.csv')
-tests = DailyReports[1][2:]
+tests = DailyReports[2][2:]
 
 update_date = str(int(month)) + "/" + str(int(day)-1) + "/" +str(year)
 
@@ -48,15 +48,51 @@ if all_tests[all_tests["date"].str.match(update_date)].shape[0]==0:
              all_tests.columns[2]: int(tests[tests.columns[1]].iloc[0].replace(".","")),
              all_tests.columns[3]: int(tests[tests.columns[1]].iloc[1].replace(".","")),
              all_tests.columns[4]: int(tests[tests.columns[1]].iloc[2].replace(".","")),
-             all_tests.columns[5]: int(tests[tests.columns[1]].iloc[0].replace(".","")),
-             all_tests.columns[6]: int(tests[tests.columns[1]].iloc[1].replace(".","")),
-             all_tests.columns[7]: int(tests[tests.columns[1]].iloc[2].replace(".",""))}, ignore_index=True)
+             all_tests.columns[5]: int(tests[tests.columns[3]].iloc[0].replace(".","")),
+             all_tests.columns[6]: int(tests[tests.columns[3]].iloc[1].replace(".","")),
+             all_tests.columns[7]: int(tests[tests.columns[3]].iloc[2].replace(".",""))}, ignore_index=True)
 
     all_tests = all_tests.append(dailytest)
     directory = "C:/Users/maibe/Dropbox/covid/data/"
-    name_file = directory + "tests_updated" + str(date) + ".csv"
+    name_file = directory + "tests.csv"
 
     all_tests.to_csv(name_file, index = False)
+
+# For daily data:
+all_data = pd.read_csv('https://raw.githubusercontent.com/maibennett/code/master/covid/data/data_covid_region.csv')
+data = DailyReports[0]
+
+update_date = str(int(month)) + "/" + str(int(day)) + "/" +str(year)
+
+regions = ["Arica y Parinacota","Tarapaca","Antofagasta","Atacama", "Coquimbo", 
+              "Valparaiso", "Metropolitana","Ohiggins", "Maule","Nuble","Biobio",
+              "Araucania","Los Rios","Los Lagos","Aysen","Magallanes"]
+
+# Only if we haven't recorded that value
+if all_data[all_data["date"].str.match(update_date)].shape[0]==0:
+    dailydata = pd.DataFrame(columns=all_data.columns)
+    
+    k = 0
+    
+    for region in regions:
+        
+        all_data_subset = all_data[all_data['region'] == region] 
+        
+        dailydata = dailydata.append(
+                {all_data.columns[0]: max(all_data['day'])+1,
+                 all_data.columns[1]: region,
+                 all_data.columns[2]: update_date,
+                 all_data.columns[3]: max(all_data_subset['region_lat']),
+                 all_data.columns[4]: max(all_data_subset['region_lon']),
+                 all_data.columns[5]: data['Casos nuevos'].iloc[k],
+                 all_data.columns[6]: data['Fallecidos'].iloc[k]}, ignore_index=True)
+        k = k+1
+
+    all_data = all_data.append(dailydata)
+    directory = "C:/Users/maibe/Dropbox/covid/data/"
+    name_file = directory + "data_covid_region.csv"
+
+    all_data.to_csv(name_file, index = False)
 
 
 # For UCI:
